5e06f0ee8a5c7c6819ff46db0e67e7fe94c16d64
Your Name <n.d.russell@hotmail.com>
2022-06-07 14:03:00 +0100

    Absolute

--- sign.js
+++ sign.js
@@ -1,11 +1,10 @@
 function sign(n) {
-    switch (n) {
-        case (n > 0):
-            return 1
-        case (n == 0):
-            return 0
-        case (n < 0):
-            return -1
+    if (n > 0) {
+        return 1
+    } else if (n == 0) {
+        return 0
+    } else {
+        return -1
     }
 }
 
