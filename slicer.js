3ca94899216c9eff648cad6d1da44128fda7f68f
Your Name <n.d.russell@hotmail.com>
2022-06-07 19:31:04 +0100

    all

--- slicer.js
+++ slicer.js
@@ -1,54 +1,91 @@
-const slice = (val, num, num2) => {
-    if (Array.isArray(val)) {
-        if (num >= 0) {
-            if (num2 != null) {
-                for (let i = num; i < num2; i++) {
-                    res.push(val[i])
-                }
-                return res
-            } 
-            //create an empty slice to return the new value
-            let res = [];
-            //Loop through the array until the end, with i as num initially
-            for (let i = num; i < val.length; i++) {
-                res.push(val[i])
-            }
-            return res
-        } else {
-            let res = [];
-            //Loop through the array until the end, with i as num initially
-            for (let i = val.length - 1; i >= val.length+num ; i--) {
-                res.push(val[i])
-            }
-            return res.reverse()
-        }
-    } else {
-        if (num >= 0) {
-            if (num2 != null) {
-                let res = '';
-                //Loop through the array until the end, with i as num initially
-                for (let i = num; i < num2; i++) {
-                    res += val[i]
-                }
-                return res
-            } 
-            //create an empty slice to return the new value
-            let res = '';
-            //Loop through the array until the end, with i as num initially
-            for (let i = num; i < val.length; i++) {
-                res += val[i]
-            }
-            return res
-        } else {
-            let res = '';
-            //Loop through the array until the end, with i as num initially
-            for (let i = val.length - 1; i >= val.length + num; i--) {
-                res += val[i]
-            }
-            const split = res.split('')
-            const rev = split.reverse()
-            const join = rev.join('')
-            return join
-        }
+// const slice = (val, num, num2) => {
+//     if (Array.isArray(val)) {
+//         if (num >= 0) {
+//             if (num2 != null) {
+//                 for (let i = num; i < num2; i++) {
+//                     res.push(val[i])
+//                 }
+//                 return res
+//             }
+//             //create an empty slice to return the new value
+//             let res = [];
+//             //Loop through the array until the end, with i as num initially
+//             for (let i = num; i < val.length; i++) {
+//                 res.push(val[i])
+//             }
+//             return res
+//         } else {
+//             let res = [];
+//             //Loop through the array until the end, with i as num initially
+//             for (let i = val.length - 1; i >= val.length+num ; i--) {
+//                 res.push(val[i])
+//             }
+//             return res.reverse()
+//         }
+//     } else {
+//         if (num >= 0) {
+//             if (num2 != null) {
+//                 let res = '';
+//                 //Loop through the array until the end, with i as num initially
+//                 for (let i = num; i < num2; i++) {
+//                     res += val[i]
+//                 }
+//                 return res
+//             }
+//             //create an empty slice to return the new value
+//             let res = '';
+//             //Loop through the array until the end, with i as num initially
+//             for (let i = num; i < val.length; i++) {
+//                 res += val[i]
+//             }
+//             return res
+//         } else {
+//             let res = '';
+//             //Loop through the array until the end, with i as num initially
+//             for (let i = val.length - 1; i >= val.length + num; i--) {
+//                 res += val[i]
+//             }
+//             const split = res.split('')
+//             const rev = split.reverse()
+//             const join = rev.join('')
+//             return join
+//         }
+//     }
+// }
+
+const slice = (slc, n1, n2) => {
+    if (n1 < 0) {
+        n1 = slc.length + n1
     }
+    if ((n2 < 0) && (n2 != null)) {
+        n2 = slc.length + n2
+    }
+
+    let upto = slc.length
+
+    if (n2 != null) {
+        upto = n2
+    }
+
+    if (Array.isArray(val)) {
+
+        //create an empty slice to return the new value
+        let res = [];
+        //Loop through the array until the end, with i as num initially
+        for (let i = n1; i < upto; i++) {
+            res.push(val[i])
+        }
+        return res
+
+    } else {
+        let res = '';
+        //Loop through the array until the end, with i as num initially
+        for (let i = n1; i < upto; i++) {
+            res += val[i]
+        }
+        return res
+    }
+
+
+
 }
