0514b5ec09247c4c8701792ac894841e3ee42766
Your Name <n.d.russell@hotmail.com>
2022-06-07 15:02:04 +0100

    Absolute

--- is.js
+++ is.js
@@ -6,6 +6,6 @@ is.undef = (u) => (typeof u === 'undefined');
 is.def = (d) => !(typeof u === 'undefined');
 is.arr = (a) => (Array.isArray(a));
 is.obj = (o) => (typeof o === 'object');
-is.fun = (f) => ((typeof f === 'function')||(f === null));
+is.fun = (f) => (typeof f === 'function');
 is.truthy = (t) => (t == true);
 is.falsy = (f) => (f == false);
