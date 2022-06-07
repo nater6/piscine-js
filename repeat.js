ed17253e730f1fa4acedade1c786e105178c407a
Your Name <n.d.russell@hotmail.com>
2022-06-07 15:53:30 +0100

    all

--- repeat.js
+++ repeat.js
@@ -1,8 +1,8 @@
 function repeat(str, num) {
     let res = ''
     //loop and return the value each time
-    for (let i = 0; i < number; i++) {
-        res = res + string
+    for (let i = 0; i < num; i++) {
+        res = res + str
     }
     return res
 }
