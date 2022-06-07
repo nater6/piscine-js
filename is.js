fd767358180e3785609a8660b0daae43c20b871f
Your Name <n.d.russell@hotmail.com>
2022-06-07 14:45:29 +0100

    Absolute

--- is.js
+++ is.js
@@ -1,11 +1,11 @@
-is.num = (n) => typeof n === 'number';
+is.num = (n) => (typeof n === 'number');
 is.nan = (n) => Number.isNaN(n);
-is.str = (s) => typeof s === 'string';
-is.bool = (b) => typeof b === 'boolean'
-is.undef = (u) => typeof u === 'undefined';
+is.str = (s) => (typeof s === 'string');
+is.bool = (b) => (typeof b === 'boolean');
+is.undef = (u) => (typeof u === 'undefined');
 is.def = (d) => !(typeof u === 'undefined');
-is.arr = (a) => typeof a === 'array';
+is.arr = (a) => (Array.isArray(a));
 is.obj = (o) => (typeof o === 'object');
 is.fun = (f) => (typeof f === 'function');
-is.truthy = (t) => t == true;
-is.falsy = (f) => f == false;
+is.truthy = (t) => (t == true);
+is.falsy = (f) => (f == false);
