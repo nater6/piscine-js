bbe2fbceab8b5a231647a49cded3249e39165c0f
Your Name <n.d.russell@hotmail.com>
2022-06-07 13:55:20 +0100

    Absolute

---
+++ abs.js
@@ -0,0 +1,15 @@
+function isPositive(n) {
+    if (n > 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function abs(x) {
+    if (x >= 0) {
+        return x
+    } else {
+        return x * -1
+    }
+}
