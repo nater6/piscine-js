9cd55926c7be6a21638fd5400b38fc2a10249190
Your Name <n.d.russell@hotmail.com>
2022-06-07 13:46:57 +0100

    all

---
+++ method-man.js
@@ -0,0 +1,19 @@
+function words(str) {
+    return str.split(" ");
+}
+
+function sentence(arr) {
+    const sent = arr.join(" ");
+}
+
+function yell(str) {
+    return str.toUpperCase();
+}
+
+function whisper(str) {
+    return '*' + str.toLowerCase() + '*';
+}
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
