6129262bcec8942c0d8ea4fcf79a46c9f87d2d88
Your Name <n.d.russell@hotmail.com>
2022-06-07 19:04:21 +0100

    all

--- slicer.js
+++ slicer.js
@@ -1,18 +1,40 @@
 const slice = (val, num) => {
     if (Array.isArray(val)) {
-        //create an empty slice to return the new value
-        let res = [];
-        //Loop through the array until the end, with i as num initially
-        for (let i = num; i < val.length; i++) {
-            res.push(val[i])
+        if (number >= 0) {
+            //create an empty slice to return the new value
+            let res = [];
+            //Loop through the array until the end, with i as num initially
+            for (let i = num; i < val.length; i++) {
+                res.push(val[i])
+            }
+            return res
+        } else {
+            let res = [];
+            //Loop through the array until the end, with i as num initially
+            for (let i = val.length - 1; i >= val.length+num ; i--) {
+                res.push(val[i])
+            }
+            return res.reverse()
         }
-        return res
     } else {
-        //create an empty string to return the new value
-        let res = '';
-        for (let i = num; i < val.length; i++) {
-            res += val[i]
+        if (number >= 0) {
+            //create an empty slice to return the new value
+            let res = '';
+            //Loop through the array until the end, with i as num initially
+            for (let i = num; i < val.length; i++) {
+                res += val[i]
+            }
+            return res
+        } else {
+            let res = '';
+            //Loop through the array until the end, with i as num initially
+            for (let i = val.length - 1; i >= val.length + num; i--) {
+                res += val[i]
+            }
+            const split = res.split('')
+            const rev = split.reverse()
+            const join = rev.join('')
+            return join
         }
-        return res
     }
 }
