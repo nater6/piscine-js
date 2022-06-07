edb875c3703334351ba642a9e3426526e2580dfa
Your Name <n.d.russell@hotmail.com>
2022-06-07 17:24:07 +0100

    all

--- is.js
+++ is.js
@@ -5,7 +5,9 @@ is.bool = (b) => (typeof b === 'boolean');
 is.undef = (u) => (typeof u === 'undefined');
 is.def = (d) => !(typeof u === 'undefined');
 is.arr = (a) => (Array.isArray(a));
-is.obj = (o) => (typeof o === 'object') && (o !== null) ;
+is.obj = (o) => {
+    (typeof o === 'object') && (o !== null) && !is.arr(o);
+}
 is.fun = (f) => (typeof f === 'function');
 is.truthy = (t) => (t == true);
 is.falsy = (f) => (f == false);
