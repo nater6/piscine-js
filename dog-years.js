82a21478bc045135a40a36e79794fd802f8d7c0d
Your Name <n.d.russell@hotmail.com>
2022-06-07 12:50:46 +0100

    Working in console

--- dog-years.js
+++ dog-years.js
@@ -7,31 +7,31 @@ function dogYears(planet, age) {
             return parseFloat(earthAge.toFixed(2))
             break;
         case 'mercury':
-            mercAge = earthAge / 0.2408467
+           const mercAge = earthAge / 0.2408467
             return parseFloat(mercAge.toFixed(2))
             break;
         case 'venus':
-            venusAge = earthAge / 0.61519726
+            const venusAge = earthAge / 0.61519726
             return parseFloat(venusAge.toFixed(2))
             break;
         case 'mars':
-            marsAge = earthAge / 1.8808158
+            const marsAge = earthAge / 1.8808158
             return parseFloat(marsAge.toFixed(2))
             break;
         case 'jupiter':
-            jupAge = earthAge / 11.862615
+            const jupAge = earthAge / 11.862615
             return parseFloat(jupAge.toFixed(2))
             break;
         case 'saturn':
-            satAge = earthAge / 29.447498
+            const satAge = earthAge / 29.447498
             return parseFloat(satAge.toFixed(2))
             break;
         case 'uranus':
-            urAge = earthAge / 84.016846
+            const urAge = earthAge / 84.016846
             return parseFloat(urAge.toFixed(2))
             break;
         case 'neptune':
-            nepAge = earthAge / 164.79132
+            const nepAge = earthAge / 164.79132
             return parseFloat(nepAge.toFixed(2))
             break;
     }
