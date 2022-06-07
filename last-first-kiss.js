c27344989aa52b99b7781218cfa920e04d7f8c06
Your Name <n.d.russell@hotmail.com>
2022-06-07 11:24:06 +0100

    Working in console

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,14 +1,15 @@
 function first(arg) {
     return
 }
+
 function last(arg) {
     var x = arg.length
     return arg[x - 1]
 }
 
 function kiss(arg) {
-    var f = arg[0]
-    var l = arg[arg.length - 1]
+    const f = first(arg)
+    const l = last(arg)
     const res = [f, l]
     return res
 }
