002d784c8abd0f855cd0fdd17a54177706e94025
Your Name <n.d.russell@hotmail.com>
2022-06-07 17:15:03 +0100

    all

---
+++ index-of.js
@@ -0,0 +1,34 @@
+const indexOf = (arr, val) => {
+
+
+    if (start != null) {
+        let i = start
+    } else {
+        let i = 0
+    }
+    //loop thorugh the array
+    while (i < arr.length) {
+        if (arr[i] == val) {
+            return i
+        }
+        i++
+    }
+    return -1
+}
+
+const lastIndexOf = (arr, val) => {
+    //reverse the array
+    var revArr = arr.reverse();
+    //call index of on revArr
+    res = indexOf(revArr, val)
+    return res
+}
+
+const includes = (arr, val) => {
+    let check = indexOf(arr, val)
+    if (check == -1) {
+        return false
+    } else {
+        return true
+    }
+}
