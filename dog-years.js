c3c6ea4761192abdb2befb2628bbbb759808fa74
Your Name <n.d.russell@hotmail.com>
2022-06-07 12:33:03 +0100

    Freeze and consts reset

--- dog-years.js
+++ dog-years.js
@@ -3,34 +3,34 @@ function dogYears(planet, age) {
     const earthAge = age / 31557600
 
     switch (planet) {
-        case earth:
+        case 'earth':
             return earthAge.toFixed(2)
             break;
-        case mercury:
+        case 'mercury':
             mercAge = earthAge / 0.2408467
             return mercAge.toFixed(2)
             break;
-        case venus:
+        case 'venus':
             venusAge = earthAge / 0.61519726
             return venusAge.toFixed(2)
             break;
-        case mars:
+        case 'mars':
             marsAge = earthAge / 1.8808158
             return marsAge.toFixed(2)
             break;
-        case jupiter:
+        case 'jupiter':
             jupAge = earthAge / 11.862615
             return jupAge.toFixed(2)
             break;
-        case saturn:
+        case 'saturn':
             satAge = earthAge / 29.447498
             return satAge.toFixed(2)
             break;
-        case uranus:
+        case 'uranus':
             urAge = earthAge / 84.016846
             return urAge.toFixed(2)
             break;
-        case neptune:
+        case 'neptune':
             nepAge = earthAge / 164.79132
             return nepAge.toFixed(2)
             break;
