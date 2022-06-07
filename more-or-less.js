b19bfb75aec0005623c661cd8c72a625a6108edd
Your Name <n.d.russell@hotmail.com>
2022-06-07 10:54:13 +0100

    Working in console

---
+++ more-or-less.js
@@ -0,0 +1,18 @@
+function more(increment) {
+    increment++
+
+}
+
+function less(decrement) {
+    decrement--
+
+}
+
+
+function add(sum1, sum2) {
+    sum1 + sum2
+}
+
+function sub(diff1, diff2) {
+    diff1 - diff2
+}
