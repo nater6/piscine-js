298e7808a5319701fc5d6b4f5267bbe4016ab9ad
Your Name <n.d.russell@hotmail.com>
2022-06-08 11:37:40 +0100

    all

---
+++ keep-cut.js
@@ -0,0 +1,19 @@
+const cutFirst = (str) => str.slice(2) 
+
+const cutLast = (str) => str.slice(0, (str.length - 2))
+
+const cutFirstLast = (str) => str.slice(2, (str.length - 2))
+
+const KeepFisrt = (str) => str.slice(0, 2)
+
+const keepLast = (str) => str.slice(-2)
+
+const keepFirstLast = (str) => {
+    //Empty string to store result
+    let res = ''
+    //add first to values to the string
+    res = str.slice(0, 2)
+    //Add last two values to the string
+    res += str.slice(-2)
+    return res
+}
