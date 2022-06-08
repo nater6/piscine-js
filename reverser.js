b33712db1648380cde5b70c1a715fc9a6c453b03
Your Name <n.d.russell@hotmail.com>
2022-06-08 16:26:24 +0100

    all

--- reverser.js
+++ reverser.js
@@ -1,16 +1,15 @@
 const reverse = (val) => {
-    if (val === 'string') {
+    if (Array.isArray(val)) {
+        let res = [];
+        for (let i = val.length - 1; i >= 0; i--) {
+            res.push(val[i]);
+        }
+        return res;
+    } else {
         let res = "";
         for (let i = val.length - 1; i >= 0; i--) {
             res += val[i];
         }
         return res;
-    } else {
-        let res = [];
-        for (let i = val.length - 1; i >= 0; i--) {
-            res.push(val[i])
-        }
-        return res;
     }
-    
 };
