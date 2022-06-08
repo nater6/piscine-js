904bfd2c1f41193a6683e3108d2aaf5c02efe588
Your Name <n.d.russell@hotmail.com>
2022-06-08 18:53:59 +0100

    all

---
+++ chunky.js
@@ -0,0 +1,15 @@
+const chunk = (arr, n) => {
+    let res, temp = [];
+    let add = n-1
+
+    for (let i = 0; i < arr.length; i++) {
+        temp.push(arr[i])
+
+        if ((i == add) || (i == arr.length - 1)) {
+            res.push(temp)
+            temp = [];
+            add += n
+        }
+        
+    }
+}
