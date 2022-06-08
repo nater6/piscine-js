14c311460aa0ade0ea85bdb4a16c684236c32d90
Your Name <n.d.russell@hotmail.com>
2022-06-08 12:39:58 +0100

    all

--- elementary.js
+++ elementary.js
@@ -1,15 +1,15 @@
 const multiply = (a, b) => {
-   // check if a and b are negative
+    // check if a and b are negative
     const aSign = Math.sign(a)
     const bSign = Math.sign(b)
 
     //make a and b absolute values
     a = Math.abs(a)
     b = Math.abs(b)
-    
+
     //Calculate the multiplied value
     var res = 0
-    for (let i = 0; i < a; i++){
+    for (let i = 0; i < a; i++) {
         res += b
     }
     if (aSign != bSign) {
@@ -28,8 +28,8 @@ const divide = (a, b) => {
     a = Math.abs(a)
     b = Math.abs(b)
 
-   
-    
+
+
     let div = 0
     let i = 0
     while (i < a) {
@@ -38,8 +38,8 @@ const divide = (a, b) => {
     }
     div--
 
-    if (aSign != bSign) {
-        return parseFloat(-div) 
+    if ((aSign == bSign) == false) {
+        return parseFloat(-div)
     } else {
         return parseFloat(div)
     }
