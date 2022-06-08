3b57c63e3902938a3140e6e4d390b5315637e02d
Your Name <n.d.russell@hotmail.com>
2022-06-08 19:57:01 +0100

    all

--- triangle.js
+++ triangle.js
@@ -1,8 +1,8 @@
-const triangle = (n) => {
+const triangle = (x, n) => {
     let res = ''
     for (let i = 0; i <= n; i++) {
         for (let j = 0; j < i; j++) {
-            res += '*'
+            res += x
             if (j == i - 1) {
                 res += '\n'
             }
