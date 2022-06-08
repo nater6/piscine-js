bdd147e59b720a2e72214dadfebd042295b3a9b8
Your Name <n.d.russell@hotmail.com>
2022-06-08 19:46:14 +0100

    all

--- get.js
+++ get.js
@@ -1,5 +1,13 @@
 var get = function (obj, path) {
-    for (var i = 0, path = path.split("."), len = path.length; i < len; i++) {
+    path = path.split(".")
+
+    for (let i = 0; i < path.length; i++) {
+        if (path[i] == null) {
+            return undefined
+        }
+    }
+    
+    for (var i = 0, len = path.length; i < len; i++) {
         obj = obj[path[i]];
     }
     return obj;
