303c36ced4ac9ecc9252e59a959793058b352eff
Your Name <n.d.russell@hotmail.com>
2022-06-08 12:24:01 +0100

    all

--- cut-corners.js
+++ cut-corners.js
@@ -1,19 +1,29 @@
 const round = (n) => {
     switch (n) {
-        case (Number.NaN):
+        case (n !== 'number'):
             return 0
         case (n <= Integer.MIN_VALUE):
             return Integer.MIN_VALUE
         case (n >= Integer.MAX_VALUE):
             return Integer.MAX_VALUE
         default:
-            return parseFloat(n.toFixed())   
-    }   
+            return parseFloat(n.toFixed())
+    }
 }
 
-const ceil = (n) => parseFloat((n + 0.499999999999999).toFixed())
+const ceil = (n) => {
+    if (n !== 'number') {
+        return 0
+    }
+    parseFloat((n + 0.499999999999999).toFixed())
+}
 
-const floor = (n) => parseFloat((n - 0.499999999999999).toFixed())
+const floor = (n) => {
+    if (n !== 'number') {
+        return 0
+    }
+    parseFloat((n - 0.499999999999999).toFixed())
+}
 
 const trunc = (n) => {
     switch (n) {
@@ -23,6 +33,6 @@ const trunc = (n) => {
             return ceil(n);
         case (n > 0):
             return floor(n);
-        
+
     }
 }
