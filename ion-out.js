3f5d7f27ee59f9c1731a43407058952c18083e14
Your Name <n.d.russell@hotmail.com>
2022-06-09 14:35:47 +0100

    all

--- ion-out.js
+++ ion-out.js
@@ -1,14 +1,6 @@
 const ionOut = (str) => {
     const pattern = /\w*tion\b/g
     let temp = str.match(new RegExp(pattern))
-    if (temp == null) {
-        return [];
-    }
-    
-    let res = [];
-    for (let i = 0; i < temp.length; i++) {
-        res.push(temp[i].slice(0,-3))
-    }
-    return res
-
+    if (temp == null) return [];
+    return temp.map((x) => x.slice(0, -3))
 }
