72cd7010f0f406ea42af1fa523cafb75cf863771
Your Name <n.d.russell@hotmail.com>
2022-06-10 13:36:31 +0100

    all

---
+++ valid-ip.js
@@ -0,0 +1,21 @@
+const findIP = (str) => {
+    const validFormat = /(0(?!\d)|[1-9][0-9]{0,2})\.(0(?!\d)|[1-9][0-9]{0,2})\.(0(?!\d)|[1-9][0-9]{0,2})\.(0(?!\d)|[1-9][0-9]{0,2})(:[0-9]{1,5})?/g
+    let res = str.match(validFormat)
+    let result = [];
+    for (let i = 0; i < res.length; i++) {
+        let temp = res[i].split('.')
+        let push = true
+        for (let j = 0; j < temp.length; j++) {
+            if (temp[j] > 255) {
+                push = false
+                continue
+            }
+        }
+        if (push) {
+            result.push(res[i])
+        }
+    }
+    return result
+}
+
+
