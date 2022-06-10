c42549d0fff36ef66737479b496f5f7196422a4a
Your Name <n.d.russell@hotmail.com>
2022-06-10 11:15:52 +0100

    all

---
+++ greedy-url.js
@@ -0,0 +1,29 @@
+const getURL = (str) => {
+    const urlStart = /(https:\/\/|http:\/\/)\S+(?=\s)/g
+    const res = str.match(new RegExp(urlStart))
+    return res
+}
+
+const greedyQuery = (str) => {
+    const validUrls = getURL(str)
+    const res = [];
+
+    for (let j = 0; j < validUrls.length; j++) {
+        const greedyUrls = validUrls[j].split('&')
+        if (greedyUrls.length >= 3) res.push(validUrls[j])
+    
+    }
+    return res
+}
+
+const notSoGreedy = (str) => {
+    const validUrls = getURL(str)
+    const res = [];
+
+    for (let j = 0; j < validUrls.length; j++) {
+        const greedyUrls = validUrls[j].split('&')
+        if ((greedyUrls.length == 3) || (greedyUrls.length == 2))  res.push(validUrls[j])
+
+    }
+    return res
+}
