600abf183aab8dd6d333f4398c56cd11ec5c8f97
Your Name <n.d.russell@hotmail.com>
2022-06-14 18:54:45 +0100

    all

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,20 +1,15 @@
 function dayOfTheYear(date) {
     let feb = 28
     if (isLeapYear(date)) feb = 29
-    if (date.getFullYear() == 0 && date.getMonth() == 11) {
-        date.setMonth(0)
-        date.setDate(1)
-        return 1
-    }
+    console.log(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate())
 
     let monthDays = [31, feb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     let days = 0
-    if (date.getFullYear() < 1850 && date.getFullYear() > 0) days++
-    console.log(date.toString())
-    for (let i = 0; i < date.getMonth(); i++) {
+
+    for (let i = 0; i < date.getUTCMonth(); i++) {
         days += monthDays[i]
     }
-    return days + date.getDate()
+    return days + date.getUTCDate()
 
 }
 
