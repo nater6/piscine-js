ce92ce33c9d9f0c115efb31e0357e2cd6052fa30
Your Name <n.d.russell@hotmail.com>
2022-06-14 09:39:30 +0100

    all

--- get-some-time.js
+++ get-some-time.js
@@ -1,9 +1,44 @@
-function firstDayWeek(week, year) {
-    let monday = new Date(year, 0, (1 + (week - 1) * 7))
 
-    while (monday.getDay != 1) {
-        monday.setDate(monday.getDate - 1)
+function firstDayWeek(week, year) {
+    let date = (1 + ((week - 1) * 7))
+
+    let monday = new Date(year, 0, date )
+    console.log(monday)
+
+    while (monday.getDay() !== 1) {
+        if (monday.getFullYear() == year - 1) {
+            return '01-01-' + year.toString()
+        }
+        monday.setDate(monday.getDate() - 1)
     }
 
-    return monday.getDate().toString() + '-' + (monday.getMonth() + 1).toString() + '-' + monday.getFullYear().toString()
+    if (year.toString().slice(0, 2) == '00') {
+        monday.setDate(monday.getDate() + 1)
+    }
+
+    let dateRes = ''
+    //format the date
+
+        let track1 = monday.getDate()
+        if (track1 < 10) {
+            dateRes += '0' + track1.toString()+'-'
+        } else {
+            dateRes += track1.toString()+'-'
+        }
+
+    // format the month
+    if (monday.getMonth() +1  < 10) {
+        dateRes += '0' + (monday.getMonth()+ 1).toString()+'-'
+    } else {
+        dateRes += (monday.getMonth() + 1).toString()+'-'
+    }
+    
+    //format the year
+    if (year.toString().slice(0, 2) == '00') {
+        dateRes += '00' + monday.getFullYear().toString().slice(-2)
+    } else {
+        dateRes += monday.getFullYear().toString()
+    }
+    return dateRes
 }
+
