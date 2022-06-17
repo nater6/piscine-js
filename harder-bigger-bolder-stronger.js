41e11588f7b32697a93b0b1a9e29fc2388dfae74
Your Name <n.d.russell@hotmail.com>
2022-06-17 11:30:55 +0100

    all

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,47 @@
+let idVal = 1
+let fontSizeNum = 11
+
+
+export function generateLetters() {
+
+    function newLetterdiv() {
+
+        //create a new div and give it a value
+        const newDiv = document.createElement("div");
+        newDiv.setAttribute('id', idVal.toString())
+        idVal++
+
+        //Choose a random letter from A-Z
+        const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        const newContent = document.createTextNode(alpha[Math.floor(Math.random() * alpha.length)]);
+        // const newContent = document.createTextNode("Hi there and greetings!");
+        // add the text node to the newly created div
+        newDiv.append(newContent);
+        newDiv.style.fontSize = fontSizeNum.toString() + 'px'
+        fontSizeNum++
+
+        switch (idVal) {
+            case (idVal <= 40):
+                newDiv.style.fontWeight = 300
+                
+            case (idVal <= 80):
+                newDiv.style.fontWeight = 400
+            case (idVal > 80):
+                newDiv.style.fontWeight = 600
+
+        }
+
+        // add the newly created element and its content into the DOM
+        const currentDiv = document.getElementById((idVal - 1).toString());
+        document.body.insertBefore(newDiv, currentDiv);
+    }
+
+    
+
+    while (idVal <= 120) {
+        newLetterdiv()
+    }
+
+
+    
+}
