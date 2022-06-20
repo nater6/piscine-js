196a2d7332f1c3370e221274db4affdd511a1190
Your Name <n.d.russell@hotmail.com>
2022-06-20 18:15:22 +0100

    pick-omit Done

--- pick-omit.js
+++ pick-omit.js
@@ -1,14 +1,23 @@
 function pick(obj, str) {
-
     let result = {};
     Object.keys(obj).forEach(key => {
-        if (str === 'string') {
-          if (str === (key)) result[key] = obj[key]
+        if (typeof str === 'string') {
+            if (key === str) result[key] = obj[key]
         } else if (Array.isArray(str)) {
             if (str.includes(key)) result[key] = obj[key]
         }
-        
     });
     return result;
+}
 
+function omit(obj, str) {
+    let result = {};
+    Object.keys(obj).forEach(key => {
+        if (typeof str === 'string') {
+            if (key !== str) result[key] = obj[key]
+        } else if (Array.isArray(str)) {
+            if (!str.includes(key)) result[key] = obj[key]
+        }
+    });
+    return result;
 }
