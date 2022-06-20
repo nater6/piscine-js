9b1ef77f82167851de75217dea90d02e4c55635d
Your Name <n.d.russell@hotmail.com>
2022-06-20 13:56:14 +0100

    all

---
+++ invert.js
@@ -0,0 +1,8 @@
+function invert(obj) {
+    let result = {};
+    Object.keys(obj).forEach(key => {
+        result[obj[key]] = key
+    })
+
+    return result
+}
--- pick-and-click.js
+++ pick-and-click.js
@@ -1,31 +1,32 @@
 export function pick() {
 
     //create the div containing the hsl value
-        const hslDiv = document.createElement('div')
-    hslDiv.setAttribute('class', 'hsl')
-    hslDiv.setAttribute('class', 'text')
-    hslDiv.setAttribute('style', 'background: none; font-weight: bold;')
-        const hslValue = document.createTextNode('hsl(0, 50%, 0%)')
-        hslDiv.appendChild(hslValue)
-        const currentDiv = document.getElementById("div1");
-        document.body.insertBefore(hslDiv, currentDiv);
+    const hslDiv = document.createElement('div')
 
-        //Create the hue div
-        const hueDiv = document.createElement('div')
-    hueDiv.setAttribute('class', 'hue')
+    hslDiv.setAttribute('class', 'text hsl')
+
+    const hslValue = document.createTextNode('hsl(0, 50%, 0%)')
+    hslDiv.appendChild(hslValue)
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(hslDiv, currentDiv);
+
+    //Create the hue div
+    const hueDiv = document.createElement('div')
+    hueDiv.setAttribute('class', 'text hue')
     hueDiv.setAttribute('style', 'position: absolute;')
-        let hueValue = document.createTextNode(`hue 0`)
-        hueDiv.appendChild(hueValue)
-        const prevDiv = document.getElementById("div1");
-        document.body.insertBefore(hueDiv, prevDiv);
+    let hueValue = document.createTextNode(`hue 0`)
+    hueDiv.appendChild(hueValue)
+    const prevDiv = document.getElementById("div1");
+    document.body.insertBefore(hueDiv, prevDiv);
 
-        //Create the luminosity div
-        const lumDiv = document.createElement('div')
-    lumDiv.setAttribute('class', 'luminosity')
+    //Create the luminosity div
+    const lumDiv = document.createElement('div')
+
+    lumDiv.setAttribute('class', 'text luminosity')
     lumDiv.setAttribute('style', 'position: absolute;')
-        const lumValue = document.createTextNode('luminosity  0')
-        lumDiv.appendChild(lumValue)
-        const prev1Div = document.getElementById("div1");
+    const lumValue = document.createTextNode('luminosity  0')
+    lumDiv.appendChild(lumValue)
+    const prev1Div = document.getElementById("div1");
     document.body.insertBefore(lumDiv, prev1Div);
     var axisX = document.createElementNS('http://www.w3.org/2000/svg', 'line');
     axisX.setAttributeNS('null', 'id', 'axisX')
@@ -37,7 +38,7 @@ export function pick() {
         let x = event.clientX;
         let windowHeight = document.documentElement.clientHeight;
         let y = event.clientY;
-        console.log(x,y)
+        console.log(x, y)
 
         let hueVal = Math.round((x / windowWidth) * 360)
         let lumVal = Math.round((y / windowHeight) * 100)
@@ -46,26 +47,20 @@ export function pick() {
         lumDiv.textContent = `luminosity \n ${lumVal}`
         hslDiv.textContent = `hsl(${hueVal}, 50%, ${lumVal}%)`
         document.body.style.background = `hsl(${hueVal}, 50%, ${lumVal}%)`
-        // var axisX = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-        // var axisY = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-        // axisX.setAttributeNS('null', 'id', 'axisX')
+       
         axisX.setAttributeNS('null', 'x1', x.toString())
         axisX.setAttributeNS('null', 'x2', x.toString())
-        // axisY.setAttributeNS('null', 'id', 'axisY')
         axisY.setAttributeNS('null', 'y1', y.toString())
         axisY.setAttributeNS('null', 'y2', y.toString())
         document.body.append(axisX)
         document.body.append(axisY)
-
-        document.addEventListener('copy', event => {
-            event.clipboardData.setData('text/plain','hsl('+ hueVal.toString()+', 50%, '+ lumVal.toString() + '%)')
-        })
-       
-
-        
     })
-    
+
+    document.addEventListener('click', event => {
+        event.clipboardData.setData('text/plain', `hsl( ${hueVal.toString()}, 50%, ${lumVal.toString()}%)`)
+    })
 
 
-    
+
+
 }
