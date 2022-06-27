ec6b2c4bbdd37a0e04368cdb0426b3473dccad8d
nater68 <n.d.russell@hotmail.com>
2022-06-27 18:03:31 +0100

    Debounce Finished

--- debounce.js
+++ debounce.js
@@ -1,11 +1,17 @@
-const debounce = (func, time) => {
-    let restart
-    return function () {
-        clearTimeout(restart)
-        restart = setTimeout(func(), time)
+const debounce = (fn, delay) => {
+    let timer;
+    return function (...args) {
+        clearTimeout(timer)
+        timer = setTimeout(() => fn(...args), delay)
     }
 }
 
-const opDebounce = (func, time, leading) {
-    
+const opDebounce = (func, time, options) => {
+    let timer
+    options === undefined ? options = { leading: false } : null
+    return function (...args) {
+        if (!timer && options.leading) func(...args)
+        clearTimeout(timer)
+        timer = setTimeout(() => func(...args), time)
+    }
 }
